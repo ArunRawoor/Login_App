059f45469fe96c758ef9a3c6ae51ff5df335e7d9
ArunRawoor <arun12831@gmail.com>
2024-06-18 14:57:32 +0700

    just updated

--- src/Components/NewComponent.js
+++ src/Components/NewComponent.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NewComponent() {
   return (
-    <div>This is My NewComponent Just Created </div>
+    <div>This is My NewComponent Just Created and the just checking    </div>
   )
 }
 
