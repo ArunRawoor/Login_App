2f17e21bc2ecb6292201257def67e154d25f66ad
ArunRawoor <arun12831@gmail.com>
2024-08-09 14:55:45 +0700

    this is commit demo work

--- src/Components/NewComponent.js
+++ src/Components/NewComponent.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function NewComponent() {
   return (
-    <div>This is My NewComponent Just Created and the just checking    </div>
+    <div>  
+      hii just for demo
+    </div>
   )
 }
 
